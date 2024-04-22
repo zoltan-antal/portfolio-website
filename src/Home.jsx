778533c7f012ad4f12fbc4c66ad123a2532231c1
zolantal <zoltan_antal@outlook.com>
2024-04-22 16:23:38 +0100

    Add initial introduction to Home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,26 @@
 const Home = () => {
   return (
     <>
-      <h2>Home</h2>
+      <p>
+        Hey there! I&apos;m Zoltan, a self-taught software developer
+        specialising in full-stack web development. With a background in
+        Aerospace Engineering I bring a unique blend of analytical thinking and
+        creative problem-solving to every project.
+      </p>
+      <p>
+        My web development toolkit includes React for front-end, ExpressJS for
+        back-end, and MongoDB and PostgreSQL for database management. Whether
+        it&apos;s building engaging user-interfaces or designing scalable
+        backend systems, I&apos;m dedicated to delivering high-quality solutions
+        that exceed expectations.
+      </p>
+      <p>
+        In my free time, I enjoy tackling coding challenges using Python,
+        constantly seeking to hone my problem-solving skills and exploring new
+        concepts.
+      </p>
+      <button>Connect with me</button>
+      <button>See my portfolio</button>
     </>
   );
 };
