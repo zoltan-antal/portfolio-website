25a2cac08a77c9d050e9ba8e4cd17f5bee0c2a35
zolantal <zoltan_antal@outlook.com>
2024-04-22 11:45:20 +0100

    Test dynamic link to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Link, Outlet } from 'react-router-dom';
 
 const App = () => {
-  console.log(process.env.NODE_ENV);
-
   return (
     <>
       <header>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,14 @@
 const Projects = () => {
+  const domain =
+    process.env.NODE_ENV === 'production'
+      ? window.location.hostname
+      : 'zoltan-antal.dev';
+
   return (
     <>
       <h2>Projects</h2>
+      <h3>Algorithm Visualiser</h3>
+      <a href={`https://algorithms.${domain}`}>Link</a>
     </>
   );
 };
