8a9f0082927af9cf1e90defda4eff2f06c547739
zolantal <zoltan_antal@outlook.com>
2024-05-02 12:26:11 +0100

    Add initial content, layout & styling to Skills page

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,5 +1,69 @@
+import './index.css';
+import skills from '../../constants/skills';
+import technologies from '../../constants/technologies';
+
 const Skills = () => {
-  return <></>;
+  const renderTechnologyGroup = (category, familiarity) => {
+    return (
+      <div className="technology-group">
+        {skills
+          .filter(
+            (skill) =>
+              skill.category === category && skill.familiarity === familiarity
+          )
+          .map((skill) => {
+            const technology = technologies[skill.technology];
+
+            return (
+              <div key={skill.technology} className="technology">
+                <img
+                  className="logo"
+                  src={technology.icon}
+                  alt={`${technology.name} logo`}
+                />
+                <span className="name">
+                  {technology.name.split(' ').join('\n')}
+                </span>
+              </div>
+            );
+          })}
+      </div>
+    );
+  };
+
+  return (
+    <main className="skills">
+      <h1>My Skills</h1>
+      <table>
+        <caption>Software skills</caption>
+        <thead>
+          <tr>
+            <th>Languages</th>
+            <th>Frameworks</th>
+            <th>Dev tools & Libraries</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td colSpan={3}>Proficient skills</td>
+          </tr>
+          <tr>
+            <td>{renderTechnologyGroup('languages', 'high')}</td>
+            <td>{renderTechnologyGroup('frameworks', 'high')}</td>
+            <td>{renderTechnologyGroup('tools', 'high')}</td>
+          </tr>
+          <tr>
+            <td colSpan={3}>Familiar skills</td>
+          </tr>
+          <tr>
+            <td>{renderTechnologyGroup('languages', 'low')}</td>
+            <td>{renderTechnologyGroup('frameworks', 'low')}</td>
+            <td>{renderTechnologyGroup('tools', 'low')}</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
 };
 
 export default Skills;
--- src/constants/technologies.js
+++ src/constants/technologies.js
@@ -1,23 +1,101 @@
+import apolloIcon from '../assets/images/icons/technologies/apollo.png';
 import axiosIcon from '../assets/images/icons/technologies/axios.svg';
+import bashIcon from '../assets/images/icons/technologies/bash.svg';
+import bootstrapIcon from '../assets/images/icons/technologies/bootstrap.svg';
+import csharpIcon from '../assets/images/icons/technologies/csharp.svg';
+import csharpIconTransparent from '../assets/images/icons/technologies/csharp-transparent.svg';
+import cssIcon from '../assets/images/icons/technologies/css.svg';
+import cssIconTransparent from '../assets/images/icons/technologies/css-transparent.svg';
+import cypressIcon from '../assets/images/icons/technologies/cypress.svg';
+import dockerIcon from '../assets/images/icons/technologies/docker.svg';
+import eslintIcon from '../assets/images/icons/technologies/eslint.svg';
 import expressIcon from '../assets/images/icons/technologies/express.png';
 import expressIconTransparent from '../assets/images/icons/technologies/express-transparent.svg';
+import gitIcon from '../assets/images/icons/technologies/git.svg';
+import githubIcon from '../assets/images/icons/technologies/github.svg';
+import githubactionsIcon from '../assets/images/icons/technologies/githubactions.svg';
+import graphqlIcon from '../assets/images/icons/technologies/graphql.svg';
+import htmlIcon from '../assets/images/icons/technologies/html.svg';
+import htmlIconTransparent from '../assets/images/icons/technologies/html-transparent.svg';
+import javaIcon from '../assets/images/icons/technologies/java.svg';
 import javascriptIcon from '../assets/images/icons/technologies/javascript.svg';
+import jestIcon from '../assets/images/icons/technologies/jest.svg';
 import jwtIcon from '../assets/images/icons/technologies/jwt.svg';
+import kubuntuIcon from '../assets/images/icons/technologies/kubuntu.svg';
+import linuxIcon from '../assets/images/icons/technologies/linux.svg';
+import lodashIcon from '../assets/images/icons/technologies/lodash.svg';
+import materialuiIcon from '../assets/images/icons/technologies/materialui.svg';
+import matlabIcon from '../assets/images/icons/technologies/matlab.png';
 import mongodbIcon from '../assets/images/icons/technologies/mongodb.svg';
 import mongooseIcon from '../assets/images/icons/technologies/mongoose.svg';
 import nodejsIcon from '../assets/images/icons/technologies/nodejs.svg';
+import npmIcon from '../assets/images/icons/technologies/npm.svg';
 import passportjsIcon from '../assets/images/icons/technologies/passportjs.png';
 import passportjsIconTransparent from '../assets/images/icons/technologies/passportjs-transparent.svg';
 import postgresqlIcon from '../assets/images/icons/technologies/postgresql.svg';
+import postmanIcon from '../assets/images/icons/technologies/postman.svg';
+import pythonIcon from '../assets/images/icons/technologies/python.svg';
+import racketIcon from '../assets/images/icons/technologies/racket.svg';
 import reactIcon from '../assets/images/icons/technologies/react.svg';
 import reactrouterIcon from '../assets/images/icons/technologies/reactrouter.svg';
 import reduxIcon from '../assets/images/icons/technologies/redux.svg';
+import rubyIcon from '../assets/images/icons/technologies/ruby.svg';
+import rubyIconTransparent from '../assets/images/icons/technologies/ruby-transparent.svg';
+import sqlIcon from '../assets/images/icons/technologies/sql.png';
+import tanstackqueryIcon from '../assets/images/icons/technologies/tanstackquery.png';
 import typescriptIcon from '../assets/images/icons/technologies/typescript.svg';
+import ubuntuIcon from '../assets/images/icons/technologies/ubuntu.svg';
+import visualbasicIcon from '../assets/images/icons/technologies/visualbasic.svg';
+import visualbasicIconTransparent from '../assets/images/icons/technologies/visualbasic-transparent.svg';
+import viteIcon from '../assets/images/icons/technologies/vite.svg';
+import vscodeIcon from '../assets/images/icons/technologies/vscode.svg';
+import webpackIcon from '../assets/images/icons/technologies/webpack.svg';
+import windowsIcon from '../assets/images/icons/technologies/windows.svg';
 
 const technologies = {
+  apollo: {
+    name: 'Apollo',
+    icon: apolloIcon,
+    iconTransparent: false,
+  },
   axios: {
     name: 'Axios',
     icon: axiosIcon,
+    iconTransparent: false,
+  },
+  bash: {
+    name: 'Bash',
+    icon: bashIcon,
+    iconTransparent: false,
+  },
+  bootstrap: {
+    name: 'Bootstrap',
+    icon: bootstrapIcon,
+    iconTransparent: false,
+  },
+  csharp: {
+    name: 'C#',
+    icon: csharpIcon,
+    iconTransparent: csharpIconTransparent,
+  },
+  css: {
+    name: 'CSS',
+    icon: cssIcon,
+    iconTransparent: cssIconTransparent,
+  },
+  cypress: {
+    name: 'Cypress',
+    icon: cypressIcon,
+    iconTransparent: false,
+  },
+  docker: {
+    name: 'Docker',
+    icon: dockerIcon,
+    iconTransparent: false,
+  },
+  eslint: {
+    name: 'ESLint',
+    icon: eslintIcon,
     iconTransparent: false,
   },
   express: {
@@ -25,14 +103,74 @@ const technologies = {
     icon: expressIcon,
     iconTransparent: expressIconTransparent,
   },
+  git: {
+    name: 'Git',
+    icon: gitIcon,
+    iconTransparent: false,
+  },
+  github: {
+    name: 'GitHub',
+    icon: githubIcon,
+    iconTransparent: false,
+  },
+  githubactions: {
+    name: 'GitHub Actions',
+    icon: githubactionsIcon,
+    iconTransparent: false,
+  },
+  graphql: {
+    name: 'GraphQL',
+    icon: graphqlIcon,
+    iconTransparent: false,
+  },
+  html: {
+    name: 'HTML',
+    icon: htmlIcon,
+    iconTransparent: htmlIconTransparent,
+  },
+  java: {
+    name: 'Java',
+    icon: javaIcon,
+    iconTransparent: false,
+  },
   javascript: {
     name: 'JavaScript',
     icon: javascriptIcon,
     iconTransparent: false,
   },
+  jest: {
+    name: 'Jest',
+    icon: jestIcon,
+    iconTransparent: false,
+  },
   jwt: {
     name: 'JWT',
     icon: jwtIcon,
+    iconTransparent: false,
+  },
+  kubuntu: {
+    name: 'Kubuntu',
+    icon: kubuntuIcon,
+    iconTransparent: false,
+  },
+  linux: {
+    name: 'Linux',
+    icon: linuxIcon,
+    iconTransparent: false,
+  },
+  lodash: {
+    name: 'Lodash',
+    icon: lodashIcon,
+    iconTransparent: false,
+  },
+  materialui: {
+    name: 'Material UI',
+    icon: materialuiIcon,
+    iconTransparent: false,
+  },
+  matlab: {
+    name: 'Matlab',
+    icon: matlabIcon,
     iconTransparent: false,
   },
   mongodb: {
@@ -50,6 +188,11 @@ const technologies = {
     icon: nodejsIcon,
     iconTransparent: false,
   },
+  npm: {
+    name: 'npm',
+    icon: npmIcon,
+    iconTransparent: false,
+  },
   passportjs: {
     name: 'Passport.js',
     icon: passportjsIcon,
@@ -58,6 +201,21 @@ const technologies = {
   postgresql: {
     name: 'PostgreSQL',
     icon: postgresqlIcon,
+    iconTransparent: false,
+  },
+  postman: {
+    name: 'Postman',
+    icon: postmanIcon,
+    iconTransparent: false,
+  },
+  python: {
+    name: 'Python',
+    icon: pythonIcon,
+    iconTransparent: false,
+  },
+  racket: {
+    name: 'Racket',
+    icon: racketIcon,
     iconTransparent: false,
   },
   react: {
@@ -75,9 +233,54 @@ const technologies = {
     icon: reduxIcon,
     iconTransparent: false,
   },
+  ruby: {
+    name: 'Ruby',
+    icon: rubyIcon,
+    iconTransparent: rubyIconTransparent,
+  },
+  sql: {
+    name: 'SQL',
+    icon: sqlIcon,
+    iconTransparent: false,
+  },
+  tanstackquery: {
+    name: 'TanStack Query',
+    icon: tanstackqueryIcon,
+    iconTransparent: false,
+  },
   typescript: {
     name: 'TypeScript',
     icon: typescriptIcon,
+    iconTransparent: false,
+  },
+  ubuntu: {
+    name: 'Ubuntu',
+    icon: ubuntuIcon,
+    iconTransparent: false,
+  },
+  visualbasic: {
+    name: 'Visual Basic',
+    icon: visualbasicIcon,
+    iconTransparent: visualbasicIconTransparent,
+  },
+  vite: {
+    name: 'Vite',
+    icon: viteIcon,
+    iconTransparent: false,
+  },
+  vscode: {
+    name: 'VS\u00A0Code',
+    icon: vscodeIcon,
+    iconTransparent: false,
+  },
+  webpack: {
+    name: 'Webpack',
+    icon: webpackIcon,
+    iconTransparent: false,
+  },
+  windows: {
+    name: 'Windows',
+    icon: windowsIcon,
     iconTransparent: false,
   },
 };
