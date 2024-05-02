04773c46df183df3b3435b10a7943c22fb9bef91
zolantal <zoltan_antal@outlook.com>
2024-05-02 15:53:17 +0100

    Add placeholder content, layout & styling to About page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,6 +1,51 @@
+import './index.css';
+import { Link } from 'react-router-dom';
+import wipIcon from '../../assets/images/icons/wip.svg';
+
 const About = () => {
   return (
-    <>
+    <main className="about">
+      <h1>About me</h1>
+      <div className="info">
+        <div className="introduction">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem.
+          </p>
+          <p>
+            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis
+            autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+            nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+            voluptas nulla pariatur?
+          </p>
+        </div>
+        <img className="photo" src={wipIcon} alt="Portrait" />
+      </div>
+      <div className="page-links">
+        <Link to="/skills">
+          <button>View my skillset</button>
+        </Link>
+        <Link to="/projects">
+          <button>Take a look at my work</button>
+        </Link>
+      </div>
       {/* <p>
         Hey there! I&apos;m Zoltan, a full-stack software developer specialising
         in web development. With a background in Aerospace Engineering I bring a
@@ -19,7 +64,7 @@ const About = () => {
         constantly seeking to hone my problem-solving skills and explore new
         concepts.
       </p> */}
-    </>
+    </main>
   );
 };
 
