82ae02b24c1ef8467d46ad4690828f3507ed8aec
zolantal <zoltan_antal@outlook.com>
2024-05-09 11:18:29 +0100

    Add scroll-to-top behaviour when switching routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Header></Header>
