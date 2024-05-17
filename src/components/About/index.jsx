f1a760ad157e212bd743bf168b066397f138aa8e
zolantal <zoltan_antal@outlook.com>
2024-05-17 18:04:28 +0100

    Add about me text

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,6 +2,7 @@ import './index.css';
 import { Link } from 'react-router-dom';
 import wipIcon from '../../assets/images/icons/wip.svg';
 import cv from '../../assets/files/Zoltan_Antal-CV.pdf';
+import aboutMe from '../../constants/aboutMe';
 
 const About = () => {
   const mobileLayout =
@@ -25,33 +26,7 @@ const About = () => {
       <h1>About me</h1>
       <div className="info">
         <div className="introduction">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem.
-          </p>
-          <p>
-            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis
-            autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-            nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-            voluptas nulla pariatur?
-          </p>
+          {aboutMe}
           <div className="button-group" id="cv-buttons">
             {!mobileLayout && <button onClick={viewCV}>View my CV</button>}
             <button onClick={downloadCV}>Download my CV</button>
@@ -67,24 +42,6 @@ const About = () => {
           <button>View my skillset</button>
         </Link>
       </div>
-      {/* <p>
-        Hey there! I&apos;m Zoltan, a full-stack software developer specialising
-        in web development. With a background in Aerospace Engineering I bring a
-        unique blend of analytical thinking and creative problem-solving to
-        every project.
-      </p>
-      <p>
-        My web development toolkit includes React for front-end, ExpressJS for
-        back-end, and MongoDB & PostgreSQL for database management. Whether
-        it&apos;s building engaging user-interfaces or designing scalable
-        backend systems, I&apos;m dedicated to delivering high-quality solutions
-        that exceed expectations.
-      </p>
-      <p>
-        In my free time, I enjoy tackling coding challenges using Python,
-        constantly seeking to hone my problem-solving skills and explore new
-        concepts.
-      </p> */}
     </main>
   );
 };
---
+++ src/constants/aboutMe.jsx
@@ -0,0 +1,30 @@
+const aboutMe = (
+  <>
+    <p>
+      Originally from Hungary, I moved to the UK in 2015 and graduated with a
+      Master&apos;s in Aerospace Engineering from the University of Sheffield in
+      2020. My interest in coding, sparked during my school years, grew
+      significantly whilst working as an engineer, where I found myself
+      increasingly involved in programming tasks. This rekindled passion for
+      programming ultimately led me to change career to become a software
+      developer.
+    </p>
+    <p>
+      In 2022, I began studying full-time following the computer science
+      curriculum of the Open Source Society University. This included courses in
+      algorithms & data structures, computer architecture, programming
+      languages, and other key subjects, offered by Harvard, MIT and other
+      renowned universities. In 2023, I chose to specialise in full-stack web
+      development, completing the University of Helsinki&apos;s Full Stack Open
+      programme and immersing myself in JavaScript and various backend, frontend
+      and database technologies.
+    </p>
+    <p>
+      In my free time I enjoy tackling coding challenges in Python, but beyond
+      programming my hobbies include ballroom dancing, wildlife photography,
+      hiking, wood carving and playing board games & video games.
+    </p>
+  </>
+);
+
+export default aboutMe;
